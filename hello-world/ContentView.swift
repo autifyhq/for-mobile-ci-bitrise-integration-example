80a0f74d0c004ce5160547d76a787e136a147d36
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2021-07-01 16:21:31 +0900

    Initial Commit

---
+++ hello-world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello-world
+//
+//  Created by Sota Yamashita on 2021/07/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Sota Yamashita on 2021/07/01.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
